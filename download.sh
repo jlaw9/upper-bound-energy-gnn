7982d75ce82440286cec0defd1f533a966f1a7f2
Jeff Law <jlaw@nrel.gov>
2022-09-16 16:51:43 -0600

    Fix names

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 ## Download the pretraiend models and structure files from the github release
-github_release="https://github.com/jlaw9/upper-bound-energy-gnn/releases/download/0.1"
+github_release="https://github.com/jlaw9/upper-bound-energy-gnn/releases/download/v0.1"
 
 f="fully_relaxed.json.gz"
 echo "wget -O inputs/$f $github_release/$f"
@@ -9,11 +9,11 @@ f="volume_relaxed.json.gz"
 echo "wget -O inputs/$f $github_release/$f"
 wget -O inputs/$f $github_release/$f
 
-f="icsd_full_and_vol_battery.hdf5"
+f="icsd_and_full_vol_battery.hdf5"
 echo "wget -O pretrained_models/$f $github_release/$f"
 wget -O pretrained_models/$f $github_release/$f
 
-f="20220607_icsd_full_and_vol_battery.hdf5"
+f="20220607_icsd_and_full_vol_battery.hdf5"
 echo "wget -O pretrained_models/$f $github_release/$f"
 wget -O pretrained_models/$f $github_release/$f
 
